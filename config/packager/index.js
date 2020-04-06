677bbbcf75859070f890e5f9880181b74b07e059
mihanizm56 <m727507.56@mail.ru>
2020-04-06 12:08:39 +0300

    build(scripts): build project

--- config/packager/index.js
+++ config/packager/index.js
@@ -7,8 +7,8 @@ const { exec } = require('./fs-promises');
 const fromFolder = path.join(
   process.cwd(),
   'node_modules',
-  '@mihanizm56',
-  'cli-fun-react',
+  '@@wildberries',
+  'boilerplate-cli-packager',
   'lib',
 );
 
@@ -20,11 +20,11 @@ const copier = new Copier({ arrayToCopy });
 
 const runPackage = async () => {
   try {
-    await exec('npm install @mihanizm56/cli-fun-react');
+    await exec('npm install @wildberries/boilerplate-cli-packager');
 
     copier.activate();
 
-    await exec('npm uninstall @mihanizm56/cli-fun-react');
+    await exec('npm uninstall @wildberries/boilerplate-cli-packager');
   } catch (error) {
     console.log('error when executing the package', error); // eslint-disable-line
   }
