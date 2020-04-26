35d5cdb019c47ecd13ba51f146f4ea864b5bbcc2
mihanizm56 <m727507.56@mail.ru>
2020-04-27 02:03:36 +0300

    fix commitizen

--- config/git/prepare-commit.js
+++ config/git/prepare-commit.js
@@ -5,48 +5,21 @@ const LINUX_COMMAND = 'exec < /dev/tty && git cz --hook || true';
 const WINDOWS_COMMAND = 'prepare-commit-msg';
 
 const runPrecommitExecutor = () => {
-  // eslint-disable-next-line
-  console.info(
+// eslint-disable-next-line
+console.info(
     'info',
     'runPrecommitExecutor goes on platform ',
     process.platform,
-  );
+);
 
-  const isWindows =
+const isWindows =
     process.platform === 'win32' || process.platform === 'win64';
-  const commandToExecute = isWindows ? WINDOWS_COMMAND : LINUX_COMMAND;
+const commandToExecute = isWindows ? WINDOWS_COMMAND : LINUX_COMMAND;
 
-  const child = spawn(commandToExecute, {
+spawn(commandToExecute, {
     shell: true,
-  });
-
-  child.stdout.on('data', chunk => {
-    // eslint-disable-next-line
-    console.info('info', chunk.toString());
-  });
-
-  child.stderr.on('data', chunk => {
-    // eslint-disable-next-line
-    console.info('info', chunk.toString());
-  });
-
-  child.stdout.on('error', error => {
-    // eslint-disable-next-line
-    console.info('info', 'child process get error ', error);
-    process.exit();
-  });
-
-  child.stderr.on('error', error => {
-    // eslint-disable-next-line
-    console.info('info', 'child process get error ', error);
-    process.exit();
-  });
-
-  child.on('close', code => {
-    // eslint-disable-next-line
-    console.info('info', `child process exited with code ${code}`);
-    process.exit();
-  });
+    stdio: 'inherit',
+});
 };
 
 runPrecommitExecutor();
