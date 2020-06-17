770b89460d94396f9aa38ed9591c05d8f275482e
mihanizm56 <m727507.56@mail.ru>
2020-06-17 18:10:22 +0300

    build(configure-package): upd

--- lib/ru/command-app/interface.jsx
+++ lib/ru/command-app/interface.jsx
@@ -5,7 +5,6 @@ const SelectInput = require('ink-select-input').default;
 const Spinner = require('ink-spinner').default;
 const { devServerLog } = require('../../utils/dev-server-logger');
 const { scriptExecute } = require('../_utils/cli-utils/run-executor-script');
-const { processKiller } = require('../_utils/cli-utils/process-killer');
 const {
   mainCommands,
   testCommands,
@@ -41,8 +40,6 @@ class Interface extends React.PureComponent {
     if (Boolean(this.childProcess)) {
       process.kill(-this.childProcess.pid);
     }
-
-    await processKiller();
   }
 
   componentDidCatch(error) {
