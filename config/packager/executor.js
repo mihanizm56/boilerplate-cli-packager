363d73132bf0160769fb6c8dbbf2f91f488cd496
mihanizm56 <m727507.56@mail.ru>
2020-10-16 06:45:26 +0300

    build(configure-package): update

--- config/packager/executor.js
+++ config/packager/executor.js
@@ -2,8 +2,6 @@
 
 const path = require('path');
 const Copier = require('@mihanizm56/node-file-copier');
-const colors = require('colors');
-const cliProgress = require('cli-progress');
 const { exec } = require('./utils/fs-promises');
 const { getConsoleArgs } = require('./utils/get-args');
 const { packageJsonPatch } = require('./utils/package-json-patch');
@@ -27,30 +25,15 @@ const arrayToCopy = [{ from: fromFolder, to: toFolder }];
 
 const copier = new Copier({ arrayToCopy });
 
-const cliProgressBar = new cliProgress.SingleBar({
-  format: `CLI Progress |${colors.magenta(
-    '{bar}',
-  )}| {percentage}% || Config-packager execution`,
-  barCompleteChar: '\u2588',
-  barIncompleteChar: '\u2591',
-});
-
-const cliRunner = cliProgressBar.create(100, 0);
-cliRunner.update(20);
-
 const runPackage = async () => {
   try {
     await exec('npm install @wildberries/boilerplate-cli-packager');
-    cliRunner.update(40);
 
     copier.activate();
-    cliRunner.update(60);
 
     await packageJsonPatch(configFolderPrefix);
-    cliRunner.update(80);
 
     await exec('npm uninstall @wildberries/boilerplate-cli-packager');
-    cliRunner.update(100);
   } catch (error) {
     console.log('error when executing the package', error);
     process.exit(1);
