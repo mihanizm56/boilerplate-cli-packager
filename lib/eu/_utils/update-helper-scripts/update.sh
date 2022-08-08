591f425248a2bbe1fdb5e04a762b6d8dbda871b0
mihanizm56 <m727507.56@mail.ru>
2022-08-09 01:06:18 +0500

    build(configure-package): update

--- lib/eu/_utils/update-helper-scripts/update.sh
+++ lib/eu/_utils/update-helper-scripts/update.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-rm -fr config
-rm -fr cli
-rm -fr integration-tests
+rm -fr config &&
+rm -fr cli &&
+rm -fr integration-tests &&
+rm -fr node_modules &&
+
+npm uninstall -D \ 
+    @typescript-eslint/eslint-plugin \
+    @typescript-eslint/parser \
+    eslint \
+    eslint-config-airbnb \
+    eslint-config-prettier \
+    eslint-config-react-app \
+    eslint-import-resolver-alias
+    eslint-import-resolver-typescript
+    eslint-loader \
+    eslint-plugin-css-modules \
+    eslint-plugin-flowtype \
+    eslint-plugin-import \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-prettier \
+    eslint-plugin-react \
+    eslint-plugin-react-hooks \
+    eslint-plugin-security \
+    eslint-plugin-unused-imports \
+    prettier &&
+
+npm uninstall \
+    compression-webpack-plugin \
+    customize-cra-react-refresh \
+    mini-css-extract-plugin \
+    compress \
+    react-dev-utils &&
 
 npx @wildberries/boilerplate-config-packager --euro &&
 npx @wildberries/boilerplate-cli-packager --euro &&
